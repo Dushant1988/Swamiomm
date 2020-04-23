760f25c82dbeadbbb6f156bca2e18218592ff6e3
Dushant1988 <dushant.jawalkar.com>
2020-04-23 13:54:42 +0530

    Swamiomm

---
+++ src/main/java/djmvnprojects/App.java
@@ -0,0 +1,13 @@
+package djmvnprojects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/djmvnprojects/SeleniumTest.java
@@ -0,0 +1,20 @@
+package djmvnprojects;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+@Test	
+public void Selmethod1()
+{
+	System.out.println("Selmethod1");
+}
+
+@Test	
+public void Selmethod2()
+{
+	System.out.println("Selmethod2");
+}
+
+
+}
